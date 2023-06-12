54e4c98dae0569d5a8ca10009d655ecb29881ba6
‘abhixsh’ <aloka.abishek9@gmail.com>
2023-06-12 15:52:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Add smooth scrolling to navigation links
+const navLinks = document.querySelectorAll("nav a");
+
+for (const link of navLinks) {
+  link.addEventListener("click", smoothScroll);
+}
+
+function smoothScroll(event) {
+  event.preventDefault();
+  const targetId = this.getAttribute("href");
+  const targetElement = document.querySelector(targetId);
+  window.scrollTo({
+    top: targetElement.offsetTop,
+    behavior: "smooth"
+  });
+}
